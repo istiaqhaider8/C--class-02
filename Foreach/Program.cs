5a345055c5c16b3d68a642b1ab6c247489936710
istiaqhaider8 <istiaqhaider8@gmail.com>
2022-07-05 11:37:04 +0600

    .net code

---
+++ Foreach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input value: ");
+            int input =  Convert.ToInt32(Console.ReadLine());
+            
+            int[] numbers = new int[input];
+
+            Console.WriteLine("Please Enter your User Input: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                //int userInput = Convert.ToInt32(Console.ReadLine());
+                String userInputText = Console.ReadLine();
+                int userInput = Convert.ToInt32(userInputText);
+                numbers[i] = userInput;
+            }
+
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number = numbers[i];
+                result = result + number;
+
+            }
+
+            Console.WriteLine("Result: "+result);
+            Console.ReadKey();
+        }
+    }
+}
